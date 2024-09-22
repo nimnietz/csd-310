212baf106663b84a9700123a93246b2502ad0666
nietz3 <tzydev@gmail.com>
2024-09-21 22:30:36 -0400

    modded test

--- module-6/mysql_test.py
+++ module-6/mysql_test.py
@@ -1,12 +1,17 @@
 import mysql.connector
 from mysql.connector import errorcode
+import os
 
+user = os.getenv("DB_USER")
+password = os.getenv("DB_PASSWORD")
+host = os.getenv("DB_HOST", "127.0.0.1")
+database = os.getenv("DB_NAME", "movies")
 
 config = {
-    "user": "root",  # or username used
-    "password": "666Sql666!",  
-    "host": "127.0.0.1",
-    "database": "movies",
+    "user": user,  # Use the user from environment variable
+    "password": password,  # Use the password from environment variable
+    "host": host,
+    "database": database,
     "raise_on_warnings": True
 }
 
